2f1352eb230719602cd3ace89f030589eaf7ec10
Arius <arius1987@gmail.com>
2023-02-16 03:45:23 +0300

    C#, DZВЯ9

---
+++ 66/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int sum(int m, int n)
+{
+if (m==n)
+    return 0;
+else
+    return m + sum(m+1, n);
+}
+System.Console.WriteLine(sum(2, 5));
+
